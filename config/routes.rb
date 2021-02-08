6022c62ca37194c61b79a5e890e88b132dc3d342
高岡拓 <takaokataku@takaokatakunoMacBook-Pro.local>
2021-02-08 19:52:55 +0900

    Add staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations',
   }
-  root 'pages#index'
-  get  'pages/show'
-  resources :users, only: [:index, :show]
+  root   'static_pages#home'
+  get    '/home',    to: 'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  
+  resources :users, only: [:show]
 end
